8e28475ad97eb306c00f4b0ff188e5fde6575688
maheshray1221 <mr5832388@gmail.com>
2025-09-02 20:13:34 +0530

    add all file

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,14 +1,45 @@
 import { use, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 function Todo() {
-    let [todos, setTodos] = useState([]);
+    let [todos, setTodos] = useState([{ task: "food", id: uuidv4() }]);
     let [newTodo, setNewTodo] = useState("");
     let addTask = () => {
-        setTodos([...todos,newTodo]);
+        setTodos((prevTodos) => {
+            return [...prevTodos, { task: newTodo, id: uuidv4() }]
+        });
         setNewTodo("");
     }
     let updateTodo = (event) => {
         setNewTodo(event.target.value);
     }
+    let DeleteTodo = (id) => {
+        setTodos((prevTodos) => todos.filter((prevTodos) => prevTodos.id != id));
+    }
+    let upperCase = () => {
+        setTodos((prevTodos) => (
+            prevTodos.map((ele) => {
+                return {
+                    ...ele,
+                    task: ele.task.toUpperCase(),
+                };
+            })
+        ))
+    };
+    let upperCaseOne = (id) => {
+        setTodos((prevTodos) =>
+            prevTodos.map((ele) => {
+                if (ele.id === id) {
+                    return {
+                        ...ele,
+                        task: ele.task.toUpperCase(),
+                    };
+                } else {
+                    return ele;
+                }
+
+            })
+        )
+    };
     return (
         <>
             <h2>Your Todo App</h2>
@@ -18,9 +49,23 @@ function Todo() {
             <button onClick={addTask}>Add</button>
             <hr />
             <h4>Your All Task</h4>
-            <ul>{todos.map((el)=>{
-                return <li>{el}</li>
-                })}</ul>
+            <ul>{todos.map((el) => {
+                return <li key={el.id} ><span>{el.task}</span>
+                    &nbsp; &nbsp; &nbsp;
+
+                    if (task === done) {
+                        <i class="fa-solid fa-circle-check"></i>
+                    }else{
+                        <i class="fa-regular fa-circle-check"></i>
+                    }
+                    <button onClick={() => DeleteTodo(el.id)} >Delete</button>
+                    &nbsp; &nbsp;
+                    <button onClick={() => upperCaseOne(el.id)} >Update</button>
+
+                </li>
+            })}
+            </ul>
+            <button onClick={upperCase}>Update</button>
         </>
     )
 }
